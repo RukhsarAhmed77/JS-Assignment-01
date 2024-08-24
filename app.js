0224414efdd8e8d6f7bb5ae1e30733a629e6a687
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-08-25 00:02:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//Assignment 01
+
+//Alert
+
+// Question 01
+
+alert("Error! Please enter a valid Password")
+
+// Question 02
+
+alert("Welcome to JS land.... \nHappy Code")
+
+// Question 03
+
+alert("Welcome to JS land")
+alert("Happy Coding")
+
+///Variables of string
+
+// Question 01
+var username
+
+// Question 02
+
+var myName= ("Rukhsar Ahmed")
+
+// Question 03
+
+var message= ("Hello World")
+alert("hello world")
+
+//Question 04
+
+alert("John Doe")
+alert("15 years old")
+alert("Certied Mobile Application Development")
+
+// Question 05
+
+alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// Question 06
+
+var email ="My Email Address is rukhsarahmed77@gmail.com"
+alert(email)
+
+// Question 07
+
+var book ="A Smarter Way To Learn Javascript"
+alert(book)
+
+// Question 08
+
+var message ="Yah! I can write HTML content through Javascript"
+document.write(message)
